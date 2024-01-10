eb6b7becfaa35930cc8f8c5ca4b70d0721be8b17
Tanmay Wani <97271195+TANMAY-WANI@users.noreply.github.com>
2024-01-10 19:08:49 +0000

    10th Jan 2024 DSA_linked_list

---
+++ LinkList/revLL.java
@@ -0,0 +1,43 @@
+package LinkList;
+
+class Node {
+
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+    }
+}
+
+public class revLL {
+
+    static Node stdApproach(Node head){
+                // code here
+                Node prev = null;
+                Node curr = head;
+                Node fwd = head.next;
+                while (curr!=null){
+                    fwd = curr.next;
+                    curr.next = prev;
+                    prev = curr;
+                    curr = fwd;
+                }
+                return prev;
+    }
+    static void printLL(Node head){
+        Node curr = head;
+        while (curr!=null){
+            System.out.println(curr.data);
+            curr = curr.next;
+        }
+    }
+    public static void main(String[] args) {
+        Node fisrt = new Node(1);
+        Node sec = new Node(2);
+        fisrt.next = sec;
+        printLL(fisrt);
+        System.out.println("After reversing");
+        Node rev = stdApproach(fisrt);
+        printLL(rev);
+    }
+}
---
+++ LinkList/revLLuptoK.java
@@ -0,0 +1,57 @@
+public class revLLuptoK {
+    class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+        }
+    }
+    static Node stdApproach(Node node){
+        Node prev = null;
+        Node prev2 = null;
+        Node curr = node;
+        Node fwd = node.next;
+        int k_const = k;
+        while (curr!=null){
+            if (k>0){
+                fwd = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = fwd;
+                k--;
+            }else if (k<=0){
+                fwd = curr.next;
+                curr.next = prev2;
+                prev2 = curr;
+                curr = fwd;
+                k--;
+            }
+        }
+        Node ptr = prev;
+        while (ptr.next!=null)
+            ptr = ptr.next;
+        ptr.next = prev2;
+        return prev;
+        
+    }
+    static void printLL(Node head){
+        Node curr = head;
+        while (curr!=null){
+            System.out.println(curr.data);
+            curr = curr.next;
+        }
+    }
+    public static void main(String[] args) {
+        Node fisrt = new Node(1);
+        Node sec = new Node(2);
+        Node third = new Node(3);
+        Node fourth = new Node(4);
+        fisrt.next = sec;
+        sec.next = third;
+        third.next = fourth;
+        printLL(fisrt);
+        System.out.println("After reversing");
+        Node rev = stdApproach(fisrt);
+        printLL(rev);
+    }
+}
