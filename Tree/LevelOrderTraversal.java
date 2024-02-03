b07cb399ab239aa15ec365d0db39172509069361
TANMAY-WANI <tanmaywani145@gmail.com>
2024-02-03 19:03:40 +0530

    3 Feb 2024 DSA_Trees

---
+++ Tree/LevelOrderTraversal.java
@@ -0,0 +1,71 @@
+package Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class LevelOrderTraversal {
+    static class  Node {
+        int data;
+        Node left,right;
+        Node(int data){
+            this.data = data;
+        }
+        
+    }
+    static void stdApproach(Node root){
+        if (root == null) {
+            return;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        Stack<Node> stack = new Stack<>();
+        boolean leftToRight = true;
+
+        queue.add(root);
+
+        int level = 0;
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            level++;
+            for (int i = 0; i < levelSize; i++) {
+                Node currNode = queue.poll();
+
+                if (leftToRight) {
+                    System.out.print(currNode.data + " ");
+                } else {
+                    stack.push(currNode);
+                }
+
+                if (currNode.left != null) {
+                    queue.add(currNode.left);
+                }
+                if (currNode.right != null) {
+                    queue.add(currNode.right);
+                }
+            }
+
+            while (!stack.isEmpty()) {
+                System.out.print(stack.pop().data + " ");
+            }
+
+            if (level % 2 == 0){
+                leftToRight = !leftToRight;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        // Build a tree
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        stdApproach(root);
+        
+    }
+}
--- Tree/TreeOpreations.java
+++ Tree/TreeOpreations.java
@@ -2,6 +2,7 @@ package Tree;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public interface TreeOpreations {
     static class Node{
@@ -207,7 +208,49 @@ public interface TreeOpreations {
         }
         return sum;
     }
+        static void stdApproach(Node root){
+            if (root == null) {
+                return;
+            }
+    
+            Queue<Node> queue = new LinkedList<>();
+            Stack<Node> stack = new Stack<>();
+            boolean leftToRight = true;
+    
+            queue.add(root);
 
+            int level = 0;
+            while (!queue.isEmpty()) {
+                int levelSize = queue.size();
+                level++;
+                for (int i = 0; i < levelSize; i++) {
+                    Node currNode = queue.poll();
+    
+                    if (leftToRight) {
+                        System.out.print(currNode.data + " ");
+                    } else {
+                        stack.push(currNode);
+                    }
+    
+                    if (currNode.left != null) {
+                        queue.add(currNode.left);
+                    }
+                    if (currNode.right != null) {
+                        queue.add(currNode.right);
+                    }
+                }
+    
+                while (!stack.isEmpty()) {
+                    System.out.print(stack.pop().data + " ");
+                }
+    
+                if (level % 2 == 0){
+                    leftToRight = !leftToRight;
+                }
+                System.out.println();
+            }
+        
+        }
     public static void main(String[] args) {
         int nodes []= {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
@@ -221,5 +264,6 @@ public interface TreeOpreations {
         // System.out.println(height(root));
         // System.out.println(diameter2(root).dia);
         // System.out.println(kLevelSum(root, 3));
+        stdApproach(root);
     }
 }
